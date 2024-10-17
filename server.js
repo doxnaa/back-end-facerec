78465c4071fd4253577ba4a1bfab59ebbf1c4f73
doxnaa <dnd.corporation.inc@gmail.com>
2024-10-18 00:19:18 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const knex = require('knex')
